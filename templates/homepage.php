39457553e4bff053ae7665a604996e1d8ae8227f
Josh Cronin <jcronin@positive.co.uk>
2016-01-11 11:24:39 +0000

    Added comments

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,5 +1,7 @@
 <?php require_once('../includes/header.php'); ?>
+<!-- BEGIN PAGE -->
 
     <h1>Preconfigured Gulp environment</h1>
 
+<!-- END PAGE -->
 <?php require_once('../includes/footer.php'); ?>
